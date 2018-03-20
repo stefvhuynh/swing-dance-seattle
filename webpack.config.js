de26af28e87e5cac5fe9ef15b0ed2fe591655a29
Stefan Huynh <huynh.stefan@gmail.com>
2018-03-19 19:55:14 -0700

    Hello world with webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" })
+  ],
+  devServer: {
+    contentBase: "./public"
+  },
+  mode: "development"
+};
