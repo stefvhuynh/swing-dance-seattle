c14b011356dd3f63829dc9b6ded5b1fd3d7552b7
Stefan Huynh <huynh.stefan@gmail.com>
2018-04-29 15:23:30 -0700

    Remove subtabs

--- src/app/components/filter.js
+++ src/app/components/filter.js
@@ -18,34 +18,6 @@ class Filter extends React.Component {
     onFilterClick: PropTypes.func
   };
 
-  subTabs = {
-    dance: [
-      { display: "Dances", route: ROUTE_DANCES },
-      { display: "Events", route: ROUTE_EVENTS }
-    ],
-    learn: [
-      {
-        display: "Classes",
-        route: ROUTE_CLASSES,
-        selected: [ROUTE_HOME, ROUTE_CLASSES]
-      },
-      { display: "Workshops", route: ROUTE_WORKSHOPS }
-    ]
-  };
-
-  onLearnTab() {
-    const { currentRoute } = this.props;
-
-    return currentRoute === ROUTE_CLASSES
-      || currentRoute === ROUTE_WORKSHOPS
-      || currentRoute === ROUTE_HOME;
-  }
-
-  onDanceTab() {
-    const { currentRoute } = this.props;
-    return currentRoute === ROUTE_DANCES || currentRoute === ROUTE_EVENTS;
-  }
-
   getTabClassName(selected) {
     return classNames("flex justify-center font-white pd-sm pointer", {
       "border-bottom-thick border-off-white": selected,
@@ -53,62 +25,53 @@ class Filter extends React.Component {
     });
   }
 
-  renderSubTabs() {
-    const { currentRoute, isMobile, onFilterClick } = this.props;
-    const subTabs = this.onLearnTab() ? this.subTabs.learn : this.subTabs.dance;
-
-    const subTabItems = subTabs.map(({ display, route, selected }) => {
-      const isTabSelected = selected
-        ? selected.includes(currentRoute)
-        : route === currentRoute;
-
-      return (
-        <li className={classNames({ "fill": isMobile })} key={route}>
-          <Link
-            className={this.getTabClassName(isTabSelected)}
-            href={route}
-            onClick={onFilterClick.bind(null, route)}
-          >
-            {display}
-          </Link>
-        </li>
-      );
-    });
-
-    return (
-      <ul className="flex bg-dark-grey justify-center">
-        {subTabItems}
-      </ul>
-    );
-  }
-
   render() {
-    const { isMobile, onFilterClick } = this.props;
+    const { currentRoute, isMobile, onFilterClick } = this.props;
 
     return (
       <div className="flex column shadow">
         <ul className="flex font-lg bg-green shadow z-top justify-center">
           <li className={classNames({ "fill": isMobile })}>
             <Link
-              className={this.getTabClassName(this.onLearnTab())}
+              className={
+                this.getTabClassName(
+                  currentRoute === ROUTE_HOME || currentRoute === ROUTE_CLASSES
+                )
+              }
               href={ROUTE_CLASSES}
               onClick={onFilterClick.bind(null, ROUTE_CLASSES)}
             >
-              Learn!
+              Classes
             </Link>
           </li>
           <li className={classNames({ "fill": isMobile })}>
             <Link
-              className={this.getTabClassName(this.onDanceTab())}
+              className={this.getTabClassName(currentRoute === ROUTE_WORKSHOPS)}
+              href={ROUTE_WORKSHOPS}
+              onClick={onFilterClick.bind(null, ROUTE_CLASSES)}
+            >
+              Workshops
+            </Link>
+          </li>
+          <li className={classNames({ "fill": isMobile })}>
+            <Link
+              className={this.getTabClassName(currentRoute === ROUTE_DANCES)}
               href={ROUTE_DANCES}
               onClick={onFilterClick.bind(null, ROUTE_DANCES)}
             >
-              Dance!
+              Dances
+            </Link>
+          </li>
+          <li className={classNames({ "fill": isMobile })}>
+            <Link
+              className={this.getTabClassName(currentRoute === ROUTE_EVENTS)}
+              href={ROUTE_EVENTS}
+              onClick={onFilterClick.bind(null, ROUTE_EVENTS)}
+            >
+              Events
             </Link>
           </li>
         </ul>
-
-        {this.renderSubTabs()}
       </div>
     );
   }
