1301c9342ea93588a14d573807dee0711064dd4a
Stefan Huynh <huynh.stefan@gmail.com>
2018-12-12 19:13:26 -0800

    Bundle server code with webpack

---
+++ webpack-server.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+
+// env should take "development", "qa", or "production".
+module.exports = env => ({
+  target: "node",
+  mode: env === "production" ? "production" : "development",
+  entry: "./src/server.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "lib"),
+    libraryTarget: "commonjs2"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(env)
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: [nodeExternals()]
+});
