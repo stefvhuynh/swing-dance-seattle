fa4c0dbcfd5fc0884ecff634e615605a2663daa1
Stefan Huynh <huynh.stefan@gmail.com>
2019-02-12 19:20:04 -0800

    Add threshold to loader

--- src/app/components/page.js
+++ src/app/components/page.js
@@ -4,7 +4,7 @@ import Loader from "./loader";
 
 const Page = ({ children, loading }) => {
   return (
-    <div className="flex column align-center desktop-pd-x-md">
+    <div className="flex column align-center pd-t-md desktop-pd-x-md">
       {loading ? <Loader /> : children}
     </div>
   );
